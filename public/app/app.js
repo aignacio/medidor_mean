fee0ffaee5e02ca3f9ac12307d027785606f68d2
Ânderson Ignácio da Silva <anderson@aignacio.com>
2016-06-10 15:40:25 -0300

    Correct pad format

--- public/app/app.js
+++ public/app/app.js
@@ -18,3 +18,18 @@ hero.config(['$routeProvider',function($routeProvider) {
       controller: ''
     });
 }]);
+
+hero.filter('numberFixedLen', function () {
+  return function (n, len) {
+      var num = parseInt(n, 10);
+      len = parseInt(len, 10);
+      if (isNaN(num) || isNaN(len)) {
+          return n;
+      }
+      num = ''+num;
+      while (num.length < len) {
+          num = '0'+num;
+      }
+      return num;
+  };
+});
